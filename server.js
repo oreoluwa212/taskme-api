1108a63151d7a073c8900a816d0d913736c97bbf
oreoluwa212 <oreoluwaajayyiruth@gmail.com>
2025-07-20 13:01:05 +0200

    fix: cors fixed for frontend url

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ connectDB();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://task-me-ai.vercel.app'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
